d678292f9945cd67971f9ac64d21a210c2a7765b
Masahiro Chiba <chiba@geminium.com>
2009-10-12 14:28:44 +0900

    initial import

---
+++ lib/Acme/Coro/Suke.pm
@@ -0,0 +1,35 @@
+package Acme::Coro::Suke;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Acme::Coro::Suke -
+
+=head1 SYNOPSIS
+
+  use Acme::Coro::Suke;
+
+=head1 DESCRIPTION
+
+Acme::Coro::Suke is
+
+=head1 AUTHOR
+
+Masahiro Chiba E<lt>chiba@geminium.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
