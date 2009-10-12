1e355dc9a754b84d5754aa62ad94818d7ab9b019
Masahiro Chiba <chiba@geminium.com>
2009-10-12 15:01:55 +0900

    implement

--- lib/Acme/Coro/Suke.pm
+++ lib/Acme/Coro/Suke.pm
@@ -1,8 +1,26 @@
 package Acme::Coro::Suke;
 use strict;
 use warnings;
-use 5.00800;
+use utf8;
 our $VERSION = '0.01';
+
+use Coro;
+use Encode;
+use base qw/Exporter/;
+our @EXPORT = qw/benzo/;
+
+our $SERIF = Encode::encode('utf-8', "うわぁ…べんぞうさんの中…すごくあったかいナリぃ… \n");
+
+sub benzo(&) {
+    my $sub = shift;
+    async {
+        Coro::on_enter {
+            print $SERIF;
+        };
+        $sub->();
+    };
+}
+
 
 1;
 __END__
@@ -11,15 +29,28 @@ __END__
 
 =head1 NAME
 
-Acme::Coro::Suke -
+Acme::Coro::Suke - the only real corosuke in benzo
 
 =head1 SYNOPSIS
 
+  use Coro;
   use Acme::Coro::Suke;
+
+  benzo {
+      print "コロ助君、ワス幸せっス\n";
+      cede;
+      print "・・・\n";
+  };
+
+  print "1\n";
+  cede; # inside to benzo
+  print "2\n";
+  cede; # and again
+
 
 =head1 DESCRIPTION
 
-Acme::Coro::Suke is
+This module emulate to corosuke x benzo.
 
 =head1 AUTHOR
 
